31638e5503221bf2edc783c28759eb4a94da4642
Suraj Air <surajair@gmail.com>
2025-04-03 11:02:46 +0530

    chore: register fonts in ChaiBuilderPages component
    - Added registerFonts function to integrate custom font loading in the ChaiBuilderPages component.

--- components/builder/chaibuilder-pages.tsx
+++ components/builder/chaibuilder-pages.tsx
@@ -1,7 +1,9 @@
 "use client";
 import { registerBlocks } from "@/blocks";
+import { registerFonts } from "@/fonts";
 import ChaiBuilderPages from "@chaibuilder/pages";
 
 registerBlocks();
+registerFonts();
 
 export default ChaiBuilderPages;
