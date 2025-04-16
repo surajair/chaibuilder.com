c58de42efd96d52b6940658707a24400c6dfefa7
Suraj Air <surajair@gmail.com>
2025-04-16 10:37:40 +0530

    refactor: simplify global data provider by removing caching logic
    - Removed the unstable caching logic from the global data provider in global.ts.
    - Streamlined the return structure for global site data.

--- data/global.ts
+++ data/global.ts
@@ -1,25 +1,18 @@
 import { registerChaiGlobalDataProvider } from "@chaibuilder/pages/server";
-import { unstable_cache as nextCache } from "next/cache";
 import { cache } from "react";
 
 const globalDataProvider = cache(async ({ lang }: { lang: string }) => {
   console.log("lang", lang);
-  return await nextCache(
-    async () => {
-      return {
-        name: "Chai Builder",
-        address: "Pune, Maharashtra, India",
-        email: "support@chaibuilder.com",
-        social: {
-          facebook: "https://www.facebook.com/chaibuilder",
-          instagram: "https://www.instagram.com/chaibuilder",
-          x: "https://x.com/chaibuilder",
-        },
-      };
+  return {
+    name: "Chai Builder",
+    address: "Pune, Maharashtra, India",
+    email: "support@chaibuilder.com",
+    social: {
+      facebook: "https://www.facebook.com/chaibuilder",
+      instagram: "https://www.instagram.com/chaibuilder",
+      x: "https://x.com/chaibuilder",
     },
-    ["global-site-data"],
-    { tags: ["global-site-data"] }
-  )();
+  };
 });
 
 registerChaiGlobalDataProvider(globalDataProvider);
