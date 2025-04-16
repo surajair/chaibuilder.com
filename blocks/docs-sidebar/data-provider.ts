870df746e057ce02ee64a4888e288415b3d505a7
Suraj Air <surajair@gmail.com>
2025-04-16 16:29:05 +0530

    refactor: update docsSidebarDataProvider to destructure block parameter for improved clarity

--- blocks/docs-sidebar/data-provider.ts
+++ blocks/docs-sidebar/data-provider.ts
@@ -1,9 +1,11 @@
 import { getDocSidebarLinks } from "@/cms/notion/get-docs-links";
 import { ChaiBlock } from "@chaibuilder/pages/runtime";
 
-export const docsSidebarDataProvider = async (
-  block: ChaiBlock<{ title: string }>
-) => {
+export const docsSidebarDataProvider = async ({
+  block,
+}: {
+  block: ChaiBlock;
+}) => {
   const docSidebarLinks = await getDocSidebarLinks(block.title);
   return {
     items: docSidebarLinks,
