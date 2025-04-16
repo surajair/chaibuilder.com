d9350200ec58b060ef9344faf5b1309345f0af99
Suraj Air <surajair@gmail.com>
2025-04-16 11:03:47 +0530

    refactor: update dataProvider in docs and legal page types to destructure pageProps for improved clarity

--- page-types/docs.ts
+++ page-types/docs.ts
@@ -6,8 +6,8 @@ registerChaiPageType("docs", {
   helpText: "A documentation page.",
   dynamicSegments: "/[a-z0-9]+(?:-[a-z0-9]+)*(?:/[a-z0-9]+(?:-[a-z0-9]+)*)*$", // regex for slug. starts with / and should contain one or more segments with lowercase letters, numbers and hyphens
   dynamicSlug: "{{slug}}",
-  dataProvider: async (props, _lang, _draft, inBuilder) => {
-    const slug = props.slug;
+  dataProvider: async ({ pageProps, inBuilder }) => {
+    const slug = pageProps.slug;
     const document = await getDocContent(
       inBuilder ? "about-chai-builder" : slug.replace("/docs/", ""),
       "Documentation"
