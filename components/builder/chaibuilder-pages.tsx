1a9bdc7952fe06ce013bea50ad8e57a0b49fb4a1
Suraj Air <surajair@gmail.com>
2025-04-23 08:42:45 +0530

    feat: removed dependeny on supabase

--- components/builder/chaibuilder-pages.tsx
+++ components/builder/chaibuilder-pages.tsx
@@ -2,20 +2,12 @@
 import { registerBlocks } from "@/blocks";
 import { bluePreset, greenPreset, orangePreset } from "@/chai/theme-presets";
 import { registerFonts } from "@/fonts";
-import { useSupabaseAuth } from "@/hooks/use-supabase-auth";
-import ChaiBuilderPages, { registerChaiSidebarPanel } from "@chaibuilder/pages";
+import ChaiBuilderPages from "@chaibuilder/pages";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import dynamic from "next/dynamic";
 import { Logo } from "./logo";
-import UserAvatarMenu from "./sign-out";
 
 registerBlocks();
 registerFonts();
-registerChaiSidebarPanel("sign-out", {
-  button: UserAvatarMenu,
-  position: "bottom",
-  label: "Sign Out",
-});
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -26,31 +18,18 @@ const queryClient = new QueryClient({
   },
 });
 
-const Login = dynamic(() => import("@/components/builder/login"), {
-  ssr: false,
-});
-
 export default function ChaiBuilderPagesWrapper() {
-  const { isLoggedIn, user } = useSupabaseAuth();
-  if (!isLoggedIn) return <Login logo={<Logo width={50} height={50} />} />;
   return (
     <QueryClientProvider client={queryClient}>
       <ChaiBuilderPages
         themePresets={[
-          { orange: orangePreset },
-          { green: greenPreset },
-          { blue: bluePreset },
+          { orange: orangePreset as any },
+          { green: greenPreset as any },
+          { blue: bluePreset as any },
         ]}
         getPreviewUrl={(slug: string) => `/chai/api/preview?slug=${slug}`}
         autoSaveSupport={false}
         logo={Logo}
-        user={{
-          id: user?.id || "",
-          name: user?.name || "",
-          email: user?.email || "",
-          authToken: user?.authToken || "",
-        }}
-        onSessionExpired={() => window.location.reload()}
       />
     </QueryClientProvider>
   );
