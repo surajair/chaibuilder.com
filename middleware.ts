57555a4c20d49deab468ebfe8da0a75dd0d71b78
Suraj Air <surajair@gmail.com>
2025-05-05 13:36:50 +0530

    feat: enhance middleware and revalidate route for improved URL handling
    - Added redirection in middleware to remove '?r=1' from URLs.
    - Updated revalidate route to handle 'redirect' parameter for dynamic redirection.
    - Improved error handling and response messages in revalidate route.

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,16 @@
 import { updateSession } from "@/lib/update-session";
-import { type NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
+  // if the url has a ?r=1, then redirect to the url without the ?r=1
+  if (request.nextUrl.searchParams.has("r")) {
+    const url = new URL(request.nextUrl.href);
+    url.searchParams.delete("r");
+    return NextResponse.redirect(
+      "https://chaibuilder.com/chai/api/revalidate?redirect=true&secret=123890&paths=" +
+        url.pathname
+    );
+  }
   return await updateSession(request);
 }
 
