63b5277d8957e69d25224d4be6590117d2627ad7
Suraj Air <surajair@gmail.com>
2025-07-15 18:05:01 +0530

    fix: update SalesIQ script initialization to use dangerouslySetInnerHTML

--- app/SalesIQ.tsx
+++ app/SalesIQ.tsx
@@ -3,9 +3,12 @@ import Script from "next/script";
 export const SalesIQ = () => {
   return (
     <>
-      <Script id="zsiqscript">
-        {`window.$zoho=window.$zoho || {};$zoho.salesiq=$zoho.salesiq||{ready:function(){}}`}
-      </Script>
+      <Script
+        id="zsalesiqscript"
+        dangerouslySetInnerHTML={{
+          __html: `window.$zoho=window.$zoho || {};$zoho.salesiq=$zoho.salesiq||{ready:function(){}}`,
+        }}
+      />
       <Script
         src={`https://salesiq.zohopublic.in/widget?wc=${process.env.NEXT_PUBLIC_SALES_IQ_ID}`}
         defer
